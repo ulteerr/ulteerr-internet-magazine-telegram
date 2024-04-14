9f7d4796ff3ecf815a0859f6a4d701e4f6710418
Igor Libukin <ulterpwnz@list.ru>
2024-04-14 15:27:53 +0300

    add button, form, header

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg = window.Telegram.WebApp
+import Header from './components/Header/Header';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+  const { onToggleButton, tg } = useTelegram()
   useEffect(() => {
     tg.ready()
   })
-  const onClose = () => {
-    tg.close()
-  }
   return (
     <div className="App">
-      <button onClick={onClose}>Закрыть</button>
+      <Header />
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
