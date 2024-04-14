5132aae45a4f916fd78910a4cec81c131d974557
Igor Libukin <ulterpwnz@list.ru>
2024-04-14 15:49:25 +0300

    add product item

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import { useTelegram } from './hooks/useTelegram';
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
   const { onToggleButton, tg } = useTelegram()
@@ -11,7 +14,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path={'form'} element={<Form />} />
+      </Routes>
     </div>
   );
 }
